5925fd15598faaf3f3f8d6f09a34538a66db0a5c
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-05-14 13:01:56 +0600

    Quality section done

--- coffee-store-client/src/pages/Home.jsx
+++ coffee-store-client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Coffees from "../components/Coffees";
 import { useLoaderData } from "react-router";
 import Banner from "../components/Banner";
+import Quality from "../components/Quality";
 const Home = () => {
   const coffees = useLoaderData();
   console.log(coffees);
   return (
     <div>
       <Banner />
+      <Quality />
       <Coffees coffees={coffees} />
     </div>
   );
