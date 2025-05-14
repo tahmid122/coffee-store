1395b10488e4b0910866989e73673a79d73773e9
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-05-14 13:44:24 +0600

    Instagram section done

--- coffee-store-client/src/pages/Home.jsx
+++ coffee-store-client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Coffees from "../components/Coffees";
 import { useLoaderData } from "react-router";
 import Banner from "../components/Banner";
 import Quality from "../components/Quality";
+import Instagram from "../components/Instagram";
 const Home = () => {
   const coffees = useLoaderData();
   console.log(coffees);
@@ -11,6 +12,7 @@ const Home = () => {
       <Banner />
       <Quality />
       <Coffees coffees={coffees} />
+      <Instagram />
     </div>
   );
 };
